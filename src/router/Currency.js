6bcd7b01a555dac1399725f315040b496818fee2
HarshitDubey007 <harshitdubey1996@gmail.com>
2021-10-09 17:14:43 +0530

    create currency

--- src/router/Currency.js
+++ src/router/Currency.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const {suppoted_currency} = require('../controller/Currency');
+const {suppoted_currency, addCurrency} = require('../controller/Currency');
 const router = express.Router();
 
 router.get("/suppotedCurrency", suppoted_currency);
+router.post("/addCurrency", addCurrency);
+
 // router.post('/signup', signup);
 // router.post('/signin', signin);
 
